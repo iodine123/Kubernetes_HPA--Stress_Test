93517e9744e2be71c9fdfb0314e024e15645f194
iodine123 <iodinehanifan@gmail.com>
2025-10-26 20:11:33 +0700

    Feat!: add metrics on apps

--- Apps/green-service/app.py
+++ Apps/green-service/app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 import math
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint', 'service'])
+
 @app.route('/')
 def home():
+    REQUESTS.labels(method='GET', endpoint='/', service='Green_service').inc() 
     result = 0
-    for i in range(1, 10_000_000):
+    for i in range(1, 10_000_000): 
         result += math.sqrt(i)
     return f"Green service version 2.0 — result: {result:.2f}"
 
+# Endpoint Prometheus scrape
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8080)
