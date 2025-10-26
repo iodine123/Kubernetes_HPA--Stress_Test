9740a4b90a6cd07d70575f012d787e743710aaa7
iodine123 <iodinehanifan@gmail.com>
2025-10-26 23:19:09 +0700

    Fix:

--- grafana_install.sh
+++ grafana_install.sh
@@ -4,9 +4,11 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Prometheus
-helm install prometheus prometheus-community/prometheus \
-  --namespace monitoring \
-  --create-namespace
+helm upgrade --install kube-prometheus prometheus-community/kube-prometheus-stack \
+  -n monitoring --create-namespace
+
+# install scrape monitor
+kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/main/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
 
 # Grafana
 helm install grafana grafana/grafana
